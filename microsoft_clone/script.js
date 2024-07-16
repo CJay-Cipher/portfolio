d430a954e3341164932a5f632f7e0d716781a112
CJay <chijiokennabueze@gmail.com>
2024-07-16 06:55:09 +0100

    main page and microsoft clone files update

--- microsoft_clone/script.js
+++ microsoft_clone/script.js
@@ -1,7 +1,9 @@
-const searchBarButton = document.getElementById("head-search");
+const searchNav = document.getElementById("head-search");
+const cartNav = document.getElementById("head-cart");
+const signInNav = document.getElementById("head-sign-in");
 const hideNavElements = document.getElementsByClassName("hide-nav");
 
-searchBarButton.addEventListener("click", function () {
+searchNav.addEventListener("click", function () {
     for (let i = 0; i < hideNavElements.length; i++) {
         hideNavElements[i].style.display = "none";
     }
@@ -36,11 +38,22 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// window.addEventListener("scroll", function () {
-//     var button = document.querySelector(".head-to-top");
-//     if (window.scrollY > 0) {
-//         button.style.visibility = "visible";
-//     } else {
-//         button.style.visibility = "hidden";
-//     }
-// });
+function handleViewportChange(event) {
+    if (window.matchMedia("(max-width: 1400px)").matches) {
+        // Change the text when the viewport width is less than or equal to 1400px
+        searchNav.firstChild.textContent = "";
+        cartNav.firstChild.textContent = "";
+        signInNav.firstChild.textContent = "";
+    } else {
+        // Change the text for other viewport widths
+        searchNav.firstChild.textContent = "Search";
+        cartNav.firstChild.textContent = "Cart";
+        signInNav.firstChild.textContent = "Sign in";
+    }
+}
+
+// Call the function initially
+handleViewportChange();
+
+// Add event listener to trigger the function whenever the viewport width changes
+window.addEventListener("resize", handleViewportChange);
