f4a7f76043d4b56d8a20f3fef14abd11123ef999
CJay <chijiokennabueze@gmail.com>
2024-07-16 07:30:26 +0100

    main page and microsoft clone files update

--- microsoft_clone/script.js
+++ microsoft_clone/script.js
@@ -1,3 +1,4 @@
+const allProductsNav = document.querySelector(".all-products");
 const searchNav = document.getElementById("head-search");
 const cartNav = document.getElementById("head-cart");
 const signInNav = document.getElementById("head-sign-in");
@@ -38,7 +39,7 @@ window.addEventListener("scroll", function () {
     }
 });
 
-function handleViewportChange(event) {
+function handleViewportChange1(event) {
     if (window.matchMedia("(max-width: 1400px)").matches) {
         // Change the text when the viewport width is less than or equal to 1400px
         searchNav.firstChild.textContent = "";
@@ -52,8 +53,20 @@ function handleViewportChange(event) {
     }
 }
 
+function handleViewportChange2(event) {
+    if (window.matchMedia("(max-width: 860px)").matches) {
+        allProductsNav.innerHTML = "";
+    } else {
+        // Change the text for other viewport widths
+        allProductsNav.innerHTML =
+            'All Microsoft <img src="icons/user_icon.svg" alt="Register">';
+    }
+}
+
 // Call the function initially
-handleViewportChange();
+handleViewportChange1();
+handleViewportChange2();
 
 // Add event listener to trigger the function whenever the viewport width changes
-window.addEventListener("resize", handleViewportChange);
+window.addEventListener("resize", handleViewportChange1);
+window.addEventListener("resize", handleViewportChange2);
